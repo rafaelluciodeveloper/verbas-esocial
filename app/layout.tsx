db860eea70f3413a8453881bf20d430a639c37f2
Rafael Lucio <rafaellucio.developer@gmail.com>
2026-02-06 08:31:58 -0300

    feat: analitcs  google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,20 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-522WWMDGE9"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-522WWMDGE9');
+          `}
+        </Script>
         {children}
       </body>
     </html>
